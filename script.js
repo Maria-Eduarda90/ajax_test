ea0b568ccb2efeb063f946b45fa1f90b44fd77f0
meyh <meyh@gmail.com>
2023-10-11 21:03:02 -0300

    logic to make asynchronous request with XMLHttpRequest

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function testeAJAX(){
-    return
+function carregarImagens() {
+  var xmlHttprequest = new XMLHttpRequest();
+  var url = "https://dog.ceo/api/breeds/image/random";
+
+  xmlHttprequest.open("GET", url, true);
+
+  xmlHttprequest.onreadystatechange = () => {
+    if (xmlHttprequest.readyState == 3) {
+      console.log("Carregando o conteudo");
+    }
+    if (xmlHttprequest.readyState == 4) {
+      var jsonResponse = JSON.parse(xmlHttprequest.responseText);
+
+      console.log("Requisição Finalizada");
+      console.log("Resultado da Requisição: " + jsonResponse);
+      console.log("Resultado da Requisição: " + jsonResponse.message);
+
+      var imgDog = document.getElementById("img_dog");
+      imgDog.src = jsonResponse.message;
+    }
+  };
+
+  xmlHttprequest.send();
 }
